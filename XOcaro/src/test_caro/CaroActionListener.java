b0d3ddbcb7d58d6a219c5740f66f5a42db85e9bb
vietangel <2001040223@s.hanu.edu.vn>
2023-11-29 00:13:08 +0700

    show project

---
+++ XOcaro/src/test_caro/CaroActionListener.java
@@ -0,0 +1,18 @@
+package test_caro;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CaroActionListener {
+    private Point point;
+
+    public CaroActionListener(Point p) {
+        // TODO Auto-generated constructor stub
+        point = p;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+}
